6307265c27dc6028212c6eae909664520f24aed9
radovan paška <radovan.paska@livesport.eu>
2024-09-26 14:38:03 +0200

    clean map

--- Meowtastic/UI/Nodes/Map/MapContent/MeshMapContent.swift
+++ Meowtastic/UI/Nodes/Map/MapContent/MeshMapContent.swift
@@ -45,49 +45,40 @@ struct MeshMapContent: MapContent {
 
 	@ViewBuilder
 	private func avatar(for node: NodeInfoEntity, name: String) -> some View {
-		ZStack(alignment: .top) {
-			AvatarNode(
-				node,
-				showTemperature: true,
-				size: 48
-			)
-			.padding(.all, 8)
+		if node.isOnline {
+			ZStack(alignment: .top) {
+				AvatarNode(
+					node,
+					showTemperature: true,
+					size: 48
+				)
+				.padding(.all, 8)
 
-			if node.hopsAway >= 0 {
-				let color = color(for: node)
-				if node.hopsAway == 0 {
+				if node.hopsAway >= 0 {
+					let visible = node.hopsAway == 0
+
 					HStack(spacing: 0) {
 						Spacer()
-						Image(systemName: "eye.circle.fill")
+						Image(systemName: visible ? "eye.circle.fill" : "\(node.hopsAway).circle.fill")
 							.font(.system(size: 20))
-							.background(color)
-							.foregroundColor(color.isLight() ? .black.opacity(0.5) : .white.opacity(0.5))
-							.clipShape(Circle())
-					}
-				}
-				else {
-					HStack(spacing: 0) {
-						Spacer()
-						Image(systemName: "\(node.hopsAway).circle.fill")
-							.font(.system(size: 20))
-							.background(color)
-							.foregroundColor(color.isLight() ? .black.opacity(0.5) : .white.opacity(0.5))
+							.background(node.color)
+							.foregroundColor(node.color.isLight() ? .black.opacity(0.5) : .white.opacity(0.5))
 							.clipShape(Circle())
 					}
 				}
 			}
-		}
-		.frame(width: 64, height: 64)
-	}
-
-	private func color(for node: NodeInfoEntity) -> Color {
-		if node.isOnline {
-			return Color(
-				UIColor(hex: UInt32(node.num))
-			)
+			.frame(width: 64, height: 64)
 		}
 		else {
-			return Color.gray.opacity(0.7)
+			ZStack(alignment: .center) {
+				RoundedRectangle(cornerRadius: 4)
+					.frame(width: 12, height: 12)
+					.foregroundColor(colorScheme == .dark ? .black.opacity(0.7) : .white.opacity(0.7))
+				RoundedRectangle(cornerRadius: 2)
+					.frame(width: 8, height: 8)
+					.foregroundColor(node.color)
+			}
+			.padding(.all, 8)
 		}
 	}
 }
