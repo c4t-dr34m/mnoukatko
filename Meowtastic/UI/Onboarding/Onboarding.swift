0e38f86e9bb1c43a2c71074f9fbd387ab0842ce6
radovan <carnero@hey.com>
2024-11-03 18:14:57 +0100

    improve onboarding

--- Meowtastic/UI/Onboarding/Onboarding.swift
+++ Meowtastic/UI/Onboarding/Onboarding.swift
@@ -6,6 +6,8 @@ struct Onboarding: View {
 	private let locationManager = CLLocationManager()
 	private let notificationManager = UNUserNotificationCenter.current()
 
+	@Environment(\.colorScheme)
+	private var colorScheme: ColorScheme
 	@State
 	private var hasLocation: Bool = false
 	@State
@@ -15,112 +17,131 @@ struct Onboarding: View {
 	@Binding
 	private var done: Bool
 
+	private var hasPermissions: Bool {
+		hasLocation && hasNotifications
+	}
+
 	@ViewBuilder
 	var body: some View {
-		VStack(alignment: .center) {
-			Text("Before We Start")
-				.font(.largeTitle)
-
-			Spacer()
-
-			VStack(alignment: .leading, spacing: 8) {
-				Label {
-					Text("Location")
-						.font(.title2)
-				} icon: {
-					Image(systemName: "mappin.and.ellipse")
-						.font(.title2)
-						.frame(width: 24, height: 24)
-						.foregroundStyle(hasLocation ? Color.green : Color.gray)
-				}
-
-				HStack {
-					Text("Meowtastic would like to use your current location to determine where you are on the map, how far are other Meshtastic® nodes, and, optionally, feed your node with location updates.")
-						.font(.callout)
-						.foregroundStyle(.gray)
-						.padding(.leading, 32)
-
-					Spacer()
-				}
-
-				HStack {
-					Spacer()
-
-					Button(action: {
-						authorizeLocation()
-					}) {
-						Text(hasLocation ? "Done" : "Allow Location")
-					}
-					.disabled(hasLocation)
-					.buttonStyle(.bordered)
-					.buttonBorderShape(.capsule)
-					.controlSize(.regular)
-				}
-			}
-
-			Divider()
-
-			VStack(alignment: .leading, spacing: 8) {
-				Label {
-					Text("Notifications")
-						.font(.title2)
-				} icon: {
-					Image(systemName: "app.badge")
-						.font(.title2)
-						.frame(width: 24, height: 24)
-						.foregroundStyle(hasNotifications ? Color.green : Color.gray)
-				}
-
-				HStack {
-					Text("Meowtastic also would like to deliver you notifications in case it discovers new Meshtastic® node, or when you got a new message. You can customize which notifications you would like later in Options.")
-						.font(.callout)
-						.foregroundStyle(.gray)
-						.padding(.leading, 32)
-
-					Spacer()
-				}
-
-				HStack {
-					Spacer()
-
-					Button(action: {
-						authorizeNotifications()
-					}) {
-						Text(hasNotifications ? "Done" : "Allow Notifications")
-					}
-					.disabled(hasNotifications)
-					.buttonStyle(.bordered)
-					.buttonBorderShape(.capsule)
-					.controlSize(.regular)
-				}
-			}
-
-			Spacer()
-
-			Button(action: {
-				UserDefaults.onboardingDone = true
-				NotificationCenter.default.post(
-					name: .onboardingDone,
-					object: nil
+		ZStack {
+			Rectangle()
+				.ignoresSafeArea()
+				.overlay(
+					Image("Meowtastic")
+						.resizable()
+						.ignoresSafeArea()
+						.aspectRatio(contentMode: .fill)
 				)
 
-				done = true
-			}) {
-				Text("Continue")
-					.disabled(!hasLocation || !hasNotifications)
+			VStack(alignment: .center) {
+				Text("Before We Start")
+					.font(.largeTitle)
+
+				Spacer()
+
+				VStack {
+					VStack(alignment: .leading, spacing: 8) {
+						Label {
+							Text("Location")
+								.font(.title2)
+						} icon: {
+							Image(systemName: "mappin.and.ellipse")
+								.font(.title2)
+								.frame(width: 24, height: 24)
+								.foregroundStyle(hasLocation ? Color.green : Color.gray)
+						}
+
+						HStack {
+							Text("Meowtastic would like to use your current location to determine where you are on the map, how far are other Meshtastic® nodes, and, optionally, feed your node with location updates.")
+								.font(.callout)
+								.foregroundStyle(.gray)
+								.padding(.leading, 32)
+
+							Spacer()
+						}
+
+						HStack {
+							Spacer()
+
+							Button(action: {
+								authorizeLocation()
+							}) {
+								Text(hasLocation ? "Done" : "Allow Location")
+							}
+							.disabled(hasLocation)
+							.buttonStyle(.bordered)
+							.buttonBorderShape(.capsule)
+							.controlSize(.regular)
+						}
+					}
+
+					Divider()
+
+					VStack(alignment: .leading, spacing: 8) {
+						Label {
+							Text("Notifications")
+								.font(.title2)
+						} icon: {
+							Image(systemName: "app.badge")
+								.font(.title2)
+								.frame(width: 24, height: 24)
+								.foregroundStyle(hasNotifications ? Color.green : Color.gray)
+						}
+
+						HStack {
+							Text("Meowtastic also would like to deliver you notifications in case it discovers new Meshtastic® node, or when you got a new message. You can customize which notifications you would like later in Options.")
+								.font(.callout)
+								.foregroundStyle(.gray)
+								.padding(.leading, 32)
+
+							Spacer()
+						}
+
+						HStack {
+							Spacer()
+
+							Button(action: {
+								authorizeNotifications()
+							}) {
+								Text(hasNotifications ? "Done" : "Allow Notifications")
+							}
+							.disabled(hasNotifications)
+							.buttonStyle(.bordered)
+							.buttonBorderShape(.capsule)
+							.controlSize(.regular)
+						}
+					}
+				}
+				.padding(.all, 16)
+				.background(colorScheme == .dark ? .black.opacity(0.85) : .white.opacity(0.85))
+				.clipShape(
+					RoundedRectangle(cornerRadius: 16)
+				)
+
+				Spacer()
+
+				Button(action: {
+					UserDefaults.onboardingDone = true
+					NotificationCenter.default.post(
+						name: .onboardingDone,
+						object: nil
+					)
+
+					done = true
+				}) {
+					Text(hasPermissions ? "Continue" : "Skip")
+				}
+				.buttonStyle(GrowingButton())
 			}
-			.buttonStyle(.bordered)
-			.buttonBorderShape(.capsule)
-			.controlSize(.large)
-		}
-		.padding(.all, 16)
-		.onAppear {
-			permissionUpdateTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
-				checkAuthorizations()
+			.padding(.all, 16)
+			.onAppear {
+				permissionUpdateTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+					checkAuthorizations()
+				}
 			}
-		}
-		.onDisappear {
-			permissionUpdateTimer?.invalidate()
+			.onDisappear {
+				permissionUpdateTimer?.invalidate()
+			}
 		}
 	}
 
@@ -149,5 +170,20 @@ struct Onboarding: View {
 
 			checkAuthorizations()
 		}
+	}
+}
+
+struct GrowingButton: ButtonStyle {
+	@Environment(\.colorScheme)
+	private var colorScheme: ColorScheme
+
+	func makeBody(configuration: Configuration) -> some View {
+		configuration.label
+			.padding()
+			.background(colorScheme == .dark ? .black : .white)
+			.foregroundStyle(Color.accentColor)
+			.clipShape(Capsule())
+			.scaleEffect(configuration.isPressed ? 1.2 : 1)
+			.animation(.easeOut(duration: 0.2), value: configuration.isPressed)
 	}
 }
