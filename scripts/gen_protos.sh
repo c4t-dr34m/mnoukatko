7067692e8dc45d1843f8673023636d0eed9c14e6
radovan paška <radovan.paska@livesport.eu>
2024-11-29 15:04:27 +0100

    correct author for script file

--- scripts/gen_protos.sh
+++ scripts/gen_protos.sh
@@ -3,7 +3,7 @@
 # 
 # Mňoukátko - the Meshtastic® client
 # 
-# Copyright © 2022-2024 Garth Vander Houwen
+# Copyright © 2022-2024 Blake McAnally
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
